752316c3c1f29b19f84e63765e822a7731a00141
Aryan Sarathe <sarathearyan731@gmail.com>
2026-01-10 16:56:16 +0530

    learn done

---
+++ learn/src/components/Card.jsx
@@ -0,0 +1,13 @@
+function Card(){
+
+    const user = 'sarathe'
+    return (
+
+    <div className='card'>
+        <h1>Aryan {user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, reiciendis!</p>
+    </div>
+    )
+}
+
+export default Card;
---
+++ learn/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <h1>This is my Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
